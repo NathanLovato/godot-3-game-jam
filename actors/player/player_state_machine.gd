5f1f87e535037e23e18559c382eb4db15439c5d0
Nathan Lovato <nathan@gdquest.com>
2018-06-18 13:42:24 +0900

    Add reusable state machine base script, simplify player state machine

--- actors/player/player_state_machine.gd
+++ actors/player/player_state_machine.gd
@@ -1,71 +1,25 @@
-extends Node
-
-signal state_changed(current_state)
-
-var states_stack = []
-var current_state = null
-var _state_machine_active = true
-
-onready var states_map = {
-	"idle": $Idle,
-	"move": $Move,
-	"dash": $Dash,
-	"stagger": $Stagger,
-	"recovery": $Recovery,
-	"die": $Die,
-}
+extends "res://utils/states/state_machine.gd"
 
 func _ready():
-	for child in get_children():
-		child.connect("finished", self, "_change_state")
-
-	states_stack.push_front($Idle)
-	current_state = states_stack[0]
-	_change_state("idle")
-
-func _input(event):
-	current_state.handle_input(event)
-
-func _physics_process(delta):
-	current_state.update(delta)
-
-func _on_animation_finished(anim_name):
-	current_state._on_animation_finished(anim_name)
+	states_map = {
+		"idle": $Idle,
+		"move": $Move,
+		"dash": $Dash,
+		"stagger": $Stagger,
+		"recovery": $Recovery,
+		"die": $Die,
+	}
 
 func _change_state(state_name):
-	if state_name == "dead":
-		queue_free()
-	if not _state_machine_active:
+	if not _active:
 		return
-
-	current_state.exit()
-	
-	if state_name == "die":
-		_state_machine_active = false
-		states_stack = []
+	if state_name in ["stagger"]:
 		states_stack.push_front(states_map[state_name])
-		current_state = states_stack[0]
-		current_state.enter()
-		return
-
-	if state_name == "previous":
-		states_stack.pop_front()
-	elif state_name in ["stagger"]:
-		states_stack.push_front(states_map[state_name])
-	elif state_name == "dead":
-		queue_free()
-		return
-	else:
-		var new_state = states_map[state_name]
-		states_stack[0] = new_state
-	
-	current_state = states_stack[0]
-	if state_name != "previous":
-		current_state.enter()
-	emit_signal("state_changed", current_state)
+	._change_state(state_name)
 
 func _on_Health_health_depleted():
 	_change_state("die")
+	set_active(false)
 
 func _on_Health_took_damage(amount):
 	_change_state("stagger")
