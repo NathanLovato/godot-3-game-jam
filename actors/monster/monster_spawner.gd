d7eefe024147eb85512b077a9bb6f1e93e951cd1
Nathan Lovato <nathan@gdquest.com>
2018-06-18 15:59:48 +0900

    Add monster spawner
    It can spawn monsters remotely (under a separate YSort node in this case)

---
+++ actors/monster/monster_spawner.gd
@@ -0,0 +1,32 @@
+extends Position2D
+
+signal spawned_monster(monster_node)
+
+export(PackedScene) var Monster
+export(NodePath) var SPAWN_PATH = NodePath(".")
+export(int) var MAX_SPAWN_COUNT = 1
+
+var count = 0
+
+func _ready():
+	spawn_monster()
+
+func spawn_monster():
+	if count == MAX_SPAWN_COUNT:
+		$SpawnTimer.stop()
+		return
+
+	var new_monster = Monster.instance()
+	new_monster.global_position = global_position
+	new_monster.connect("tree_exited", self, "_on_Monster_tree_exited")
+	get_node(SPAWN_PATH).add_child(new_monster)
+	count += 1
+	
+	$SpawnTimer.start()
+
+func _on_SpawnTimer_timeout():
+	spawn_monster()
+
+func _on_Monster_tree_exited():
+	count -= 1
+	$SpawnTimer.start()
